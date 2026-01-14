3b5a5f707c50e5cc9f35287a856b75f608f7dbed
Redrov Ivan <digit4lsh4d0w@ya.ru>
2026-01-14 23:15:37 +1000

    WIP: Добавлена инициализация базы данных

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,1 +1,39 @@
 package database
+
+import (
+	"fmt"
+	"hitalent-task/internal/config"
+	"hitalent-task/internal/errors"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func NewDatabase(cfg config.DatabaseConfig) (*gorm.DB, error) {
+	if cfg.Driver == "" {
+		return nil, &errors.DatabaseError{DSN: cfg.DSN, Err: fmt.Errorf("driver is required")}
+	}
+
+	if cfg.DSN == "" {
+		return nil, &errors.DatabaseError{DSN: cfg.DSN, Err: fmt.Errorf("DSN is required")}
+	}
+
+	var db *gorm.DB
+	var err error
+
+	switch cfg.Driver {
+	case "postgres":
+		db, err = gorm.Open(postgres.Open(cfg.DSN), &gorm.Config{})
+	case "sqlite":
+		db, err = gorm.Open(sqlite.Open(cfg.DSN), &gorm.Config{})
+	default:
+		return nil, &errors.DatabaseError{DSN: cfg.DSN, Err: gorm.ErrUnsupportedDriver}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
