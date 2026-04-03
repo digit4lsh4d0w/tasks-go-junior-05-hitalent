5175bf8e47baef8f3620bc3ceed08affad4c2e95
Redrov Ivan <digit4lsh4d0w@ya.ru>
2026-04-03 20:11:57 +1000

    Изменение имен для структур репозиториев

--- internal/repository/gorm/chat.go
+++ internal/repository/gorm/chat.go
@@ -8,36 +8,36 @@ import (
 	"gorm.io/gorm"
 )
 
-type chatRepo struct {
+type chatRepository struct {
 	db  *gorm.DB
 	log log.Logger
 }
 
 func NewChatRepository(db *gorm.DB, log log.Logger) repository.ChatRepository {
 	log.Debug("Creating new chat repository", "type", "gorm")
-	return &chatRepo{db, log}
+	return &chatRepository{db, log}
 }
 
-func (r *chatRepo) FindAll() ([]model.Chat, error) {
+func (r *chatRepository) FindAll() ([]model.Chat, error) {
 	var chats []model.Chat
 	result := r.db.Find(&chats)
 	return chats, result.Error
 }
 
-func (r *chatRepo) FindByID(id uint) (*model.Chat, error) {
+func (r *chatRepository) FindByID(id uint) (*model.Chat, error) {
 	var chat model.Chat
 	result := r.db.Preload("Messages").First(&chat, id)
 	return &chat, result.Error
 }
 
-func (r *chatRepo) Create(chat *model.Chat) error {
+func (r *chatRepository) Create(chat *model.Chat) error {
 	return r.db.Create(chat).Error
 }
 
-func (r *chatRepo) Update(chat *model.Chat) error {
+func (r *chatRepository) Update(chat *model.Chat) error {
 	return r.db.Save(chat).Error
 }
 
-func (r *chatRepo) Delete(id uint) error {
+func (r *chatRepository) Delete(id uint) error {
 	return r.db.Delete(&model.Chat{}, id).Error
 }
--- internal/repository/gorm/message.go
+++ internal/repository/gorm/message.go
@@ -8,36 +8,36 @@ import (
 	"gorm.io/gorm"
 )
 
-type msgRepo struct {
+type messageRepository struct {
 	db     *gorm.DB
 	logger log.Logger
 }
 
 func NewMessageRepository(db *gorm.DB, logger log.Logger) repository.MessageRepository {
 	logger.Debug("Creating new message repository", "type", "gorm")
-	return &msgRepo{db, logger}
+	return &messageRepository{db, logger}
 }
 
-func (r *msgRepo) FindAll() ([]model.Message, error) {
+func (r *messageRepository) FindAll() ([]model.Message, error) {
 	var msgs []model.Message
 	result := r.db.Find(&msgs)
 	return msgs, result.Error
 }
 
-func (r *msgRepo) FindByID(id uint) (*model.Message, error) {
+func (r *messageRepository) FindByID(id uint) (*model.Message, error) {
 	var msg model.Message
 	result := r.db.Find(&msg, id)
 	return &msg, result.Error
 }
 
-func (r *msgRepo) Create(msg *model.Message) error {
+func (r *messageRepository) Create(msg *model.Message) error {
 	return r.db.Create(msg).Error
 }
 
-func (r *msgRepo) Update(msg *model.Message) error {
+func (r *messageRepository) Update(msg *model.Message) error {
 	return r.db.Save(msg).Error
 }
 
-func (r *msgRepo) Delete(id uint) error {
+func (r *messageRepository) Delete(id uint) error {
 	return r.db.Delete(&model.Message{}, id).Error
 }
