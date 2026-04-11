0e204aa405aa86d15e09b3488a3f95470d3a0e1f
Redrov Ivan <digit4lsh4d0w@ya.ru>
2026-04-11 16:43:50 +1000

    Добавил новые типы доменных ошибок

--- internal/model/error.go
+++ internal/model/error.go
@@ -2,4 +2,11 @@ package model
 
 import "errors"
 
-var ErrNotFound = errors.New("record not found")
+var (
+	ErrNotFound           = errors.New("record not found")
+	ErrAlreadyExists      = errors.New("record already exists")
+	ErrChatTitleIsEmpty   = errors.New("chat title is empty")
+	ErrChatTitleTooLong   = errors.New("chat title too long")
+	ErrMessageTextIsEmpty = errors.New("message text is empty")
+	ErrMessageTextTooLong = errors.New("message text too long")
+)
