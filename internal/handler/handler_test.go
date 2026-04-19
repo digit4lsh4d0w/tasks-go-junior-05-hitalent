43329c46bc3f55537d715823d264ee3fcaa9f28b
Redrov Ivan <digit4lsh4d0w@ya.ru>
2026-04-20 04:43:58 +1000

    WIP: Добавил полноценные тесты к транспортному слою

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -9,6 +9,7 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"task-5/internal/handler"
 	"task-5/internal/model"
@@ -27,6 +28,16 @@ func (m *MockChatService) CreateChat(ctx context.Context, chat *model.Chat) erro
 	return args.Error(0)
 }
 
+func (m *MockChatService) DeleteChat(ctx context.Context, id uint) error {
+	args := m.Called(ctx, id)
+	return args.Error(0)
+}
+
+func (m *MockChatService) CreateMessage(ctx context.Context, msg *model.Message) error {
+	args := m.Called(ctx, msg)
+	return args.Error(0)
+}
+
 func (m *MockChatService) GetChatWithMessages(ctx context.Context, id uint, limit int) (*model.Chat, error) {
 	args := m.Called(ctx, id, limit)
 
@@ -38,39 +49,33 @@ func (m *MockChatService) GetChatWithMessages(ctx context.Context, id uint, limi
 	return chat, args.Error(1)
 }
 
-func (m *MockChatService) DeleteChat(ctx context.Context, id uint) error {
-	args := m.Called(ctx, id)
-	return args.Error(0)
+type testEnv struct {
+	service   *MockChatService
+	logger    *slog.Logger
+	validator *validator.Validate
 }
 
-func (m *MockChatService) CreateMessage(ctx context.Context, msg *model.Message) error {
-	args := m.Called(ctx, msg)
-	return args.Error(0)
+func setupTestEnv(mockSetup func(*MockChatService)) testEnv {
+	service := new(MockChatService)
+	if mockSetup != nil {
+		mockSetup(service)
+	}
+
+	return testEnv{
+		service:   service,
+		logger:    slog.New(slog.NewTextHandler(io.Discard, nil)),
+		validator: validator.New(),
+	}
 }
 
 func TestChatHandler_CreateChat(t *testing.T) {
-	logger := slog.New(slog.NewTextHandler(io.Discard, nil))
-	validator := validator.New()
-
 	tests := []struct {
 		name           string
 		body           string
 		mockSetup      func(m *MockChatService)
 		expectedStatus int
+		expectedBody   string
 	}{
-		{
-			name: "Success",
-			body: `{"title": "Chat title"}`,
-			mockSetup: func(m *MockChatService) {
-				m.On(
-					"CreateChat",
-					mock.Anything,
-					mock.MatchedBy(func(chat *model.Chat) bool {
-						return chat.Title == "Chat title"
-					})).Return(nil).Once()
-			},
-			expectedStatus: http.StatusCreated,
-		},
 		{
 			name:           "Broken JSON",
 			body:           `{"title": "Chat title"`,
@@ -84,7 +89,7 @@ func TestChatHandler_CreateChat(t *testing.T) {
 			expectedStatus: http.StatusBadRequest,
 		},
 		{
-			name:           "Chat title too long (200+ chars)",
+			name:           "Chat title too long",
 			body:           fmt.Sprintf(`{"title": "%s"}`, strings.Repeat("a", 201)),
 			mockSetup:      func(m *MockChatService) {},
 			expectedStatus: http.StatusBadRequest,
@@ -95,39 +100,96 @@ func TestChatHandler_CreateChat(t *testing.T) {
 			mockSetup:      func(m *MockChatService) {},
 			expectedStatus: http.StatusUnprocessableEntity,
 		},
+		{
+			name: "Chat duplicate",
+			body: `{"title": "Chat title"}`,
+			mockSetup: func(m *MockChatService) {
+				m.On(
+					"CreateChat",
+					mock.Anything,
+					mock.MatchedBy(func(chat *model.Chat) bool {
+						return chat.Title == "Chat title"
+					})).Return(model.ErrAlreadyExists).Once()
+			},
+			expectedStatus: http.StatusConflict,
+		},
+		{
+			name: "Success",
+			body: `{"title": "Chat title"}`,
+			mockSetup: func(m *MockChatService) {
+				m.On(
+					"CreateChat",
+					mock.Anything,
+					mock.MatchedBy(func(chat *model.Chat) bool {
+						return chat.Title == "Chat title"
+					}),
+				).Run(func(args mock.Arguments) {
+					chat := args.Get(1).(*model.Chat)
+
+					chat.ID = 1
+					chat.CreatedAt = time.Date(2026, 4, 20, 0, 0, 0, 0, time.UTC)
+				}).Return(nil).Once()
+			},
+			expectedStatus: http.StatusCreated,
+			expectedBody:   `{"id":1, "title":"Chat title", "created_at":"2026-04-20T00:00:00Z"}`,
+		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			service := new(MockChatService)
-			tc.mockSetup(service)
+			t.Parallel()
 
-			h := handler.NewChatHandler(service, validator, logger)
+			env := setupTestEnv(tc.mockSetup)
+			h := handler.NewChatHandler(env.service, env.validator, env.logger)
 
 			mux := http.NewServeMux()
 			mux.HandleFunc("POST /chat", h.CreateChat)
 
-			req := httptest.NewRequest(http.MethodPost, "/chat", strings.NewReader(tc.body))
+			path := "/chat"
+			req := httptest.NewRequest(http.MethodPost, path, strings.NewReader(tc.body))
+			req.Header.Set("Content-Type", "application/json")
 			rec := httptest.NewRecorder()
 
 			mux.ServeHTTP(rec, req)
 
 			require.Equal(t, tc.expectedStatus, rec.Code)
-			service.AssertExpectations(t)
+
+			if tc.expectedBody != "" {
+				require.JSONEq(t, tc.expectedBody, rec.Body.String())
+			}
+
+			env.service.AssertExpectations(t)
 		})
 	}
 }
 
 func TestChatHandler_DeleteChat(t *testing.T) {
-	logger := slog.New(slog.NewTextHandler(io.Discard, nil))
-	validator := validator.New()
-
 	tests := []struct {
 		name           string
 		chatIDPath     string
 		mockSetup      func(m *MockChatService)
 		expectedStatus int
 	}{
+		{
+			name:           "Invalid ID type",
+			chatIDPath:     "abc",
+			mockSetup:      func(m *MockChatService) {},
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			name:           "Negative ID",
+			chatIDPath:     "-1",
+			mockSetup:      func(m *MockChatService) {},
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			name:       "Not found",
+			chatIDPath: "2",
+			mockSetup: func(m *MockChatService) {
+				m.On("DeleteChat", mock.Anything, uint(2)).Return(model.ErrNotFound).Once()
+			},
+			expectedStatus: http.StatusNotFound,
+		},
 		{
 			name:       "Success",
 			chatIDPath: "1",
@@ -136,39 +198,253 @@ func TestChatHandler_DeleteChat(t *testing.T) {
 			},
 			expectedStatus: http.StatusOK,
 		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			env := setupTestEnv(tc.mockSetup)
+			h := handler.NewChatHandler(env.service, env.validator, env.logger)
+
+			mux := http.NewServeMux()
+			mux.HandleFunc("DELETE /chat/{chat_id}", h.DeleteChat)
+
+			path := "/chat/" + tc.chatIDPath
+			req := httptest.NewRequest(http.MethodDelete, path, nil)
+			req.Header.Set("Content-Type", "application/json")
+			rec := httptest.NewRecorder()
+
+			mux.ServeHTTP(rec, req)
+
+			require.Equal(t, tc.expectedStatus, rec.Code)
+			env.service.AssertExpectations(t)
+		})
+	}
+}
+
+func TestChatHandler_CreateMessage(t *testing.T) {
+	tests := []struct {
+		name           string
+		chatIDPath     string
+		body           string
+		mockSetup      func(m *MockChatService)
+		expectedStatus int
+		expectedBody   string
+	}{
 		{
-			name:           "Invalid ID Type",
+			name:           "Invalid ID type",
 			chatIDPath:     "abc",
+			mockSetup:      func(m *MockChatService) {},
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			name:           "Negative ID",
+			chatIDPath:     "-1",
+			mockSetup:      func(m *MockChatService) {},
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			name:           "Empty message text",
+			chatIDPath:     "1",
+			body:           `{"text": ""}`,
+			mockSetup:      func(m *MockChatService) {},
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			name:           "Broken JSON",
+			chatIDPath:     "1",
+			body:           `{"text": ""`,
+			mockSetup:      func(m *MockChatService) {},
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			name:           "Message text too long",
+			chatIDPath:     "1",
+			body:           fmt.Sprintf(`{"text": "%s"}`, strings.Repeat("a", 5001)),
+			mockSetup:      func(m *MockChatService) {},
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			name:           "Message text consists entirely of spaces",
+			chatIDPath:     "1",
+			body:           `{"text": "    "}`,
+			mockSetup:      func(m *MockChatService) {},
+			expectedStatus: http.StatusUnprocessableEntity,
+		},
+		{
+			name:       "Not found",
+			chatIDPath: "2",
+			body:       `{"text": "Message text"}`,
+			mockSetup: func(m *MockChatService) {
+				m.On("CreateMessage", mock.Anything, mock.MatchedBy(func(msg *model.Message) bool {
+					return msg.ChatID == 2 && msg.Text == "Message text"
+				})).Return(model.ErrNotFound).Once()
+			},
+			expectedStatus: http.StatusNotFound,
+		},
+		{
+			name:       "Success",
+			chatIDPath: "1",
+			body:       `{"text": "Message text"}`,
+			mockSetup: func(m *MockChatService) {
+				m.On("CreateMessage", mock.Anything, mock.MatchedBy(func(msg *model.Message) bool {
+					return msg.ChatID == 1 && msg.Text == "Message text"
+				})).Return(nil).Once()
+			},
+			expectedStatus: http.StatusCreated,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			env := setupTestEnv(tc.mockSetup)
+			h := handler.NewChatHandler(env.service, env.validator, env.logger)
+
+			mux := http.NewServeMux()
+			mux.HandleFunc("POST /chat/{chat_id}/message", h.CreateMessage)
+
+			path := "/chat/" + tc.chatIDPath + "/message"
+			req := httptest.NewRequest(http.MethodPost, path, strings.NewReader(tc.body))
+			req.Header.Set("Content-Type", "application/json")
+			rec := httptest.NewRecorder()
+
+			mux.ServeHTTP(rec, req)
+
+			require.Equal(t, tc.expectedStatus, rec.Code)
+			env.service.AssertExpectations(t)
+		})
+	}
+}
+
+func TestChatHandler_GetAllMessages(t *testing.T) {
+	tests := []struct {
+		name           string
+		chatIDPath     string
+		limitQuery     string
+		mockSetup      func(m *MockChatService)
+		expectedStatus int
+		expectedBody   string
+	}{
+		{
+			name:           "Invalid ID type",
+			chatIDPath:     "abc",
+			limitQuery:     "limit=20",
+			mockSetup:      func(m *MockChatService) {},
+			expectedStatus: http.StatusBadRequest,
+		},
+		{
+			name:           "Negative ID",
+			chatIDPath:     "-1",
+			limitQuery:     "limit=20",
 			mockSetup:      func(m *MockChatService) {},
 			expectedStatus: http.StatusBadRequest,
 		},
 		{
 			name:       "Not Found",
 			chatIDPath: "2",
+			limitQuery: "limit=20",
 			mockSetup: func(m *MockChatService) {
-				m.On("DeleteChat", mock.Anything, uint(2)).Return(model.ErrNotFound).Once()
+				m.On("GetChatWithMessages", mock.Anything, uint(2), 20).Return(nil, model.ErrNotFound).Once()
 			},
 			expectedStatus: http.StatusNotFound,
+		},
+		{
+			name:       "Success",
+			chatIDPath: "1",
+			limitQuery: "limit=20",
+			mockSetup: func(m *MockChatService) {
+				m.On("GetChatWithMessages", mock.Anything, uint(1), 20).Return(&model.Chat{
+					ID:        1,
+					Title:     "Chat title",
+					Messages:  []model.Message{},
+					CreatedAt: time.Now(),
+				}, nil).Once()
+			},
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name:       "Without limit",
+			chatIDPath: "1",
+			limitQuery: "",
+			mockSetup: func(m *MockChatService) {
+				m.On("GetChatWithMessages", mock.Anything, uint(1), 5).Return(&model.Chat{
+					ID:        1,
+					Title:     "Chat title",
+					Messages:  []model.Message{},
+					CreatedAt: time.Now(),
+				}, nil).Once()
+			},
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name:       "Limit too big",
+			chatIDPath: "1",
+			limitQuery: "limit=100",
+			mockSetup: func(m *MockChatService) {
+				m.On("GetChatWithMessages", mock.Anything, uint(1), 20).Return(&model.Chat{
+					ID:        1,
+					Title:     "Chat title",
+					Messages:  []model.Message{},
+					CreatedAt: time.Now(),
+				}, nil).Once()
+			},
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name:       "Limit too small",
+			chatIDPath: "1",
+			limitQuery: "limit=-5",
+			mockSetup: func(m *MockChatService) {
+				m.On("GetChatWithMessages", mock.Anything, uint(1), 5).Return(&model.Chat{
+					ID:        1,
+					Title:     "Chat title",
+					Messages:  []model.Message{},
+					CreatedAt: time.Now(),
+				}, nil).Once()
+			},
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name:       "Zero limit",
+			chatIDPath: "1",
+			limitQuery: "limit=0",
+			mockSetup: func(m *MockChatService) {
+				m.On("GetChatWithMessages", mock.Anything, uint(1), 5).Return(&model.Chat{
+					ID:        1,
+					Title:     "Chat title",
+					Messages:  []model.Message{},
+					CreatedAt: time.Now(),
+				}, nil).Once()
+			},
+			expectedStatus: http.StatusOK,
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			service := new(MockChatService)
-			tc.mockSetup(service)
+			t.Parallel()
 
-			h := handler.NewChatHandler(service, validator, logger)
+			env := setupTestEnv(tc.mockSetup)
+			h := handler.NewChatHandler(env.service, env.validator, env.logger)
 
 			mux := http.NewServeMux()
-			mux.HandleFunc("DELETE /chat/{chat_id}", h.DeleteChat)
+			mux.HandleFunc("GET /chat/{chat_id}", h.GetAllMessages)
 
-			req := httptest.NewRequest(http.MethodDelete, "/chat/"+tc.chatIDPath, nil)
+			path := "/chat/" + tc.chatIDPath
+			if tc.limitQuery != "" {
+				path += "?" + tc.limitQuery
+			}
+			req := httptest.NewRequest(http.MethodGet, path, nil)
+			req.Header.Set("Content-Type", "application/json")
 			rec := httptest.NewRecorder()
 
 			mux.ServeHTTP(rec, req)
 
 			require.Equal(t, tc.expectedStatus, rec.Code)
-			service.AssertExpectations(t)
+			env.service.AssertExpectations(t)
 		})
 	}
 }
